5f6fc8abe4c000d07fae842f7da4a163f20839d3
Muhammad Sya'bani Falif <msfalif404@gmail.com>
2024-11-24 23:12:19 +0700

    Add Users model

---
+++ src/models/index.js
@@ -0,0 +1,28 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+const dbConfig = require('../config/config');
+
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+    host: dbConfig.HOST,
+    dialect: dbConfig.dialect,
+    operartorsAliases: false
+});
+
+sequelize.authenticate().then(() => {
+    console.log('Connected to the database!');
+}).catch(error => {
+    console.log(`Error: ${error}`);
+})
+
+const db = {};
+
+db.Sequelize = Sequelize
+db.sequelize = sequelize
+
+db.users = require('./users.js')(sequelize, DataTypes);
+
+db.sequelize.sync({force: false}).then(() => {
+    console.log('Re-sync done!');
+});
+
+module.exports = db;
